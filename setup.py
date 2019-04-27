478fba4b73eea5fe5d72745173e976400debdf79
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-04-27 10:40:06 -0400

    Corrected version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Predicting neurodegeneration from global proteomics',
     author='Ryan B Patterson',
     license='GPLv3',
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,6 +17,26 @@ The necessary files are:
     'Fronatl Cortex/frontal_batch 4__Proteins.txt'
     'Fronatl Cortex/frontal_batch 5__Proteins.txt'
 """
+import pandas as pd
+
 import src.data.CleanFrame as cf
 
-hold = pd.read_csv('data/external/cingulate_batch 1__Proteins.txt', usecols=['Master', 'Accession', 'Exp. q-value', 'Sum PEP Score', 'Abundance Ratio: (F1, 127N) / (F1, 126)', 'Abundance Ratio: (F1, 127N) / (F1, 126)', 'Abundance Ratio: (F1, 128N) / (F1, 126)', 'Abundance Ratio: (F1, 128C) / (F1, 126)', 'Abundance Ratio: (F1, 129N) / (F1, 126)', 'Abundance Ratio: (F1, 129C) / (F1, 126)', 'Abundance Ratio: (F1, 130N) / (F1, 126)', 'Abundance Ratio: (F1, 130C) / (F1, 126)'], sep=None, engine='python')
+hold = pd.read_csv(
+    "data/external/cingulate_batch 1__Proteins.txt",
+    usecols=[
+        "Master",
+        "Accession",
+        "Exp. q-value",
+        "Sum PEP Score",
+        "Abundance Ratio: (F1, 127N) / (F1, 126)",
+        "Abundance Ratio: (F1, 127N) / (F1, 126)",
+        "Abundance Ratio: (F1, 128N) / (F1, 126)",
+        "Abundance Ratio: (F1, 128C) / (F1, 126)",
+        "Abundance Ratio: (F1, 129N) / (F1, 126)",
+        "Abundance Ratio: (F1, 129C) / (F1, 126)",
+        "Abundance Ratio: (F1, 130N) / (F1, 126)",
+        "Abundance Ratio: (F1, 130C) / (F1, 126)",
+    ],
+    sep=None,
+    engine="python",
+)
