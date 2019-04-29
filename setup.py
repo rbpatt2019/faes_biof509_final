27b2c5dfded4111807cd8201d7f743d8a3cf32f4
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-04-29 11:46:16 -0400

    Added automatical versioning to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import find_packages, setup
+import subprocess
+
+__version__ = subprocess.check_output(["git", "describe", "--abbrev=0"]).strip()
+__version__ = str(__version__)[2:-1]
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.1',
-    description='Predicting neurodegeneration from global proteomics',
-    author='Ryan B Patterson',
-    license='GPLv3',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-mypy', 'pytest-instafail'],
+    version=__version__,
+    description="Predicting neurodegeneration from global proteomics",
+    author="Ryan B Patterson",
+    license="GPLv3",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-mypy", "pytest-instafail"],
 )
