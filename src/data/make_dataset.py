00c8cdfaaff332886a21bf0c50573287ca19357c
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-04-30 14:00:39 -0400

    Documented prep_data function in make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,9 +17,40 @@ The necessary files are:
     'Fronatl Cortex/frontal_batch 4__Proteins.txt'
     'Fronatl Cortex/frontal_batch 5__Proteins.txt'
 """
+import glob
+
 import pandas as pd
 
 import src.data.CleanFrame as cf
+
+# Find files for fronal cortex
+fronal_files = glob.iglob('data/raw/f*')
+frontal_df = cf.CleanFrame()
+
+#find files for cingulate cortex
+cingulate_files = glob.iglob('data/raw/c*')
+cingulate_df = cf.CleanFrame()
+
+def prep_data(cf):
+    """A function for project specific CleanFrame data tidying
+
+    This function calls, in order:
+        cf.clean_cols() to clean column names
+        cf.filter_by_vals() to sort only master peptides
+        pd.df.drop() to get rid of no longer necessary master column
+        pd.df.set_index() to set Accession column to index
+        pd.df.rename() to label samples
+    
+    Inputs
+    ------
+    cf: src.data.CleanFrame.CleanFrame
+        The input CleanFrame
+
+    Returns
+    -------
+    cf: src.data.CleanFrame.CleanFrame
+        The cleaned CleanFrame
+    """
 
 hold = pd.read_csv(
     "data/external/cingulate_batch 1__Proteins.txt",
@@ -39,4 +70,5 @@ hold = pd.read_csv(
     ],
     sep=None,
     engine="python",
+    B
 )
