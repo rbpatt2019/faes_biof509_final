286aa2a61f2712b66ca9c12788b874118f1e22f8
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-05-01 11:46:41 -0400

    Converted code for reading data to a function definition

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -24,32 +24,61 @@ import pandas as pd
 
 import src.data.CleanFrame as cf
 
-# Find files for fronal cortex
-frontal_files = glob.iglob("data/raw/f*")
-frontal_read = (
-    pd.read_csv(
-        i,
-        usecols=[
-            "Master",
-            "Accession",
-            "Exp. q-value",
-            "Sum PEP Score",
-            "Abundance Ratio: (F1, 127N) / (F1, 126)",
-            "Abundance Ratio: (F1, 127C) / (F1, 126)",
-            "Abundance Ratio: (F1, 128N) / (F1, 126)",
-            "Abundance Ratio: (F1, 128C) / (F1, 126)",
-            "Abundance Ratio: (F1, 129N) / (F1, 126)",
-            "Abundance Ratio: (F1, 129C) / (F1, 126)",
-            "Abundance Ratio: (F1, 130N) / (F1, 126)",
-            "Abundance Ratio: (F1, 130C) / (F1, 126)",
-        ],
-        sep=None,
-        engine="python",
+def make_data(files, usecols=None, axis=0, join='outer', keys=None):
+    """Make a full CleanFrame from multiple files
+
+    Uses glob to find all files matching files
+    Reads them into a CleanFrame, with the option to use only a subset of columns
+    Then calls pd.concat, allowing the user to specify the axis, join, and keys
+
+    By containing all data in generators, the computational load is reduced.
+
+    Inputs
+    ------
+    files: str
+        A regex strig that matches the file names to be used
+        See https://docs.python.org/3.7/library/glob.html for more info on supported regexes
+    usecols: list-like or callable, optional
+        Return a subset of columns
+        See https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+    aixs: {0/'index', 1/'columns'}, default 0
+        for pd.concat
+        The axis to concatenate along
+    join: {'inner', 'outer'}, default 'outer'
+        for pd.concat
+        How to handle indexes on other axis(es)
+    keys: sequence, optional
+        for pd.concat
+        Construct hierarchal index using the passed keys as the outermost level
+
+    Returns
+    -------
+    data: src.data.CleanFrame.CleanFrame
+        The full CleanFrame
+    """
+    # Type check files
+    if not is(files, str):
+        raise ValueError(f'files must be a str, not {type(files)}')
+    # Find files
+    paths = glob.iglob(files)
+    # Read in files, sep=None with engine='python' will auto determine delim
+    reads = (pd.read_csv(file, usecols=usecols, sep=None, engine='python') for file in paths)
+    # Convert to CleanFrame
+    cfs = (cf.CleanFrame(i) for i in reads)
+    # Clean data
+    clean = (i.prep_data() for i in cfs)
+    # Create final CleanFrame
+    data = cf.CleanFrame(
+        pd.concat(
+            clean,
+            axis=axis,
+            join=join,
+            keys=keys,
+            sort=False,
+            copy=False,
+        )
     )
-    for i in frontal_files
-)
-frontal_cfs = (cf.CleanFrame(i) for i in frontal_read)
-frontal_clean = (i.prep_data() for i in frontal_cfs)
+    return data
 
 # pd.read_csv("data/external/cingulate_batch 1__Proteins.txt",
 #     usecols=[
