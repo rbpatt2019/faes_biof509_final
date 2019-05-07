8b0a555256a2f40c16cf7115a2b02257b089930b
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-05-07 15:36:53 -0400

    Installed UMAP and updated requirements.txt

---
+++ pre_visualize.py
@@ -0,0 +1,12 @@
+"""A script for visualising data before feature engineering and training
+
+Two plots are used here - volcano and UMAP
+
+The volcano plot allows for a visualisation of those genes that are differentially expressed
+and the significance of those changes.
+
+UMAP is an alternative to tSNE plotting and is a dimensionality reduction technique 
+for visualising clusters
+"""
+
+
