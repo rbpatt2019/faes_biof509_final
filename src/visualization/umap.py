429a2758bfd084ee7f45111e8f51ff0462bd26c4
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-05-09 11:23:03 -0400

    Begin work on UMAP

---
+++ src/visualization/umap.py
@@ -0,0 +1,83 @@
+"""A script for making UMAP plots
+
+UMAP is an alternative to tSNE that has some benefits such as speed and retaining local structure
+
+This would probably be better structured in some kind of visualising class with methods
+but am currently too short on time
+"""
+import umap
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import src.data.CleanFrame as cf
+
+# Read in the data
+frontal = pd.read_pickle("data/interim/frontal_full.pkl")
+cingulate = pd.read_pickle("data/interim/cingulate_full.pkl")
+
+# Prep for umap plot - frontal
+frontal_umap = (
+    cf.CleanFrame(frontal)
+    .T.reset_index()
+    .rename_axis(columns=None)
+    .drop(columns=["level_0"])
+    .rename(columns={"level_1": "label"})
+    .filter_by_val(col="label", vals=["q_score", "pep_score"], keep=False)
+    .reset_index()
+    .drop(columns=["index"])
+)
+frontal_umap["label"] = frontal_umap["label"].str[:-1]
+pd.to_pickle(frontal_umap, "data/interim/frontal_umap.pkl")
+
+# Prep for umap plot - cingulate
+cingulate_umap = (
+    cf.CleanFrame(cingulate)
+    .T.reset_index()
+    .rename_axis(columns=None)
+    .drop(columns=["level_0"])
+    .rename(columns={"level_1": "label"})
+    .filter_by_val(col="label", vals=["q_score", "pep_score"], keep=False)
+    .reset_index()
+    .drop(columns=["index"])
+)
+cingulate_umap["label"] = cingulate_umap["label"].str[:-1]
+pd.to_pickle(cingulate_umap, "data/interim/cingulate_umap.pkl")
+
+# Set color column and plot
+cols_data = ("log2_ad", "log2_pd", "log2_adpd")
+cols_color = ("ad_color", "pd_color", "adpd_color")
+choices = [2, 0]
+for df in (frontal_volc, cingulate_volc):
+    fig, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
+    fig.suptitle(df.name)
+    fig.text(0.5, 0.05, "log2(fold change)", ha="center", va="center", fontsize=8)
+    fig.text(
+        0.05,
+        0.5,
+        "-log10(q-score)",
+        ha="center",
+        va="center",
+        rotation="vertical",
+        fontsize=8,
+    )
+    for items in zip(cols_data, cols_color, ax):
+        # Creates color column
+        conditions = [
+            (df["-log10_q"] >= 1.30103) & (df[items[0]] >= 0.5),
+            (df["-log10_q"] >= 1.30103) & (df[items[0]] <= -0.5),
+        ]
+        df[items[1]] = np.select(conditions, choices, default=1)
+        # Creates volcano plots
+        items[2].scatter(df[items[0]], df["-log10_q"], c=df[items[1]], cmap=cmap, s=1)
+        items[2].set_title(items[0].split("_")[1].upper(), fontsize=8)
+        items[2].set_xbound(lower=-2.1, upper=5.1)
+        items[2].set_xticks(np.arange(-2, 5.1, 1), minor=False)
+        items[2].set_xticks(np.arange(-2, 5.1, 0.25), minor=True)
+        items[2].set_xticklabels([-2, -1, 0, 1, 2, 3, 4, 5], fontsize=4)
+        items[2].set_ybound(lower=0, upper=6.1)
+        items[2].set_yticks(np.arange(0, 6.1, 1), minor=False)
+        items[2].set_yticks(np.arange(0, 6.1, 0.25), minor=True)
+        items[2].set_yticklabels([0, 1, 2, 3, 4, 5, 6], fontsize=4)
+    plt.savefig(f"reports/figures/{df.name}.png", dpi=600)
+    plt.close()
+print("Volcano Plots Made!")
