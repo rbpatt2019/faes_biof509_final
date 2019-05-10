fe4d02fac14a9b575e376a2f2c32a134a76679dd
rbpatt2019 <ryan.patterson.2015@gmail.com>
2019-05-10 13:48:25 -0400

    Updated volcano plots

--- src/visualization/volcano.py
+++ src/visualization/volcano.py
@@ -6,125 +6,45 @@ and the significance of those changes.
 This would probably be better structured in some kind of visualising class with methods
 but am currently too short on time
 """
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib.colors import LinearSegmentedColormap
+import src.data.CleanFrame as cf
+
+def prep_volcano(df):
+    """Prep data for volcano plots
+
+    Cleans off unnecessary columns information then aggregates by means
+    """
+    df.columns = df.columns.droplevel(0)
+    df.columns = df.columns.str.rstrip('12')
+    columns = df.columns.unique()
+    for col in columns:
+        df[f'mean_{col}'] = df[col].mean(axis=1)
+    df = df.drop(columns=columns)
+    df['mean_q_score'] = df['mean_q_score'].replace(0, 0.000001)
+    return df
 
 # Read in the data
-frontal = pd.read_pickle("data/interim/frontal_full.pkl")
-cingulate = pd.read_pickle("data/interim/cingulate_full.pkl")
+frontal = cf.CleanFrame(pd.read_pickle("data/interim/frontal_full.pkl"))
+cingulate = cf.CleanFrame(pd.read_pickle("data/interim/cingulate_full.pkl"))
 
-# Prep for volcano plot - frontal
-frontal_volc = pd.DataFrame()
-frontal_volc.name = "Frontal_Cortex_Volcano_Plots"
-frontal_volc["-log10_q"] = -np.log10(
-    frontal.xs("q_score", level=1, axis=1).mean(axis=1).replace(0, 0.000001)
-)
-frontal_volc["log2_ad"] = np.log2(
-    pd.concat(
-        (
-            frontal.xs("ad1", level=1, axis=1).mean(axis=1),
-            frontal.xs("ad2", level=1, axis=1).mean(axis=1),
-        ),
-        axis=1,
-    ).mean(axis=1)
-)
-frontal_volc["log2_pd"] = np.log2(
-    pd.concat(
-        (
-            frontal.xs("pd1", level=1, axis=1).mean(axis=1),
-            frontal.xs("pd2", level=1, axis=1).mean(axis=1),
-        ),
-        axis=1,
-    ).mean(axis=1)
-)
-frontal_volc["log2_adpd"] = np.log2(
-    pd.concat(
-        (
-            frontal.xs("adpd1", level=1, axis=1).mean(axis=1),
-            frontal.xs("adpd2", level=1, axis=1).mean(axis=1),
-        ),
-        axis=1,
-    ).mean(axis=1)
-)
-pd.to_pickle(frontal_volc, "data/interim/frontal_volcano.pkl")
+# Prep data
+frontal_volc = prep_volcano(frontal)
+cingulate_volc = prep_volcano(cingulate)
 
-# Prep for volcano plot - cingulate
-cingulate_volc = pd.DataFrame()
-cingulate_volc.name = "Anterior_Cingulate_Gyrus_Volcano_Plots"
-cingulate_volc["-log10_q"] = -np.log10(
-    cingulate.xs("q_score", level=1, axis=1).mean(axis=1).replace(0, 0.000001)
-)
-cingulate_volc["log2_ad"] = np.log2(
-    pd.concat(
-        (
-            cingulate.xs("ad1", level=1, axis=1).mean(axis=1),
-            cingulate.xs("ad2", level=1, axis=1).mean(axis=1),
-        ),
-        axis=1,
-    ).mean(axis=1)
-)
-cingulate_volc["log2_pd"] = np.log2(
-    pd.concat(
-        (
-            cingulate.xs("pd1", level=1, axis=1).mean(axis=1),
-            cingulate.xs("pd2", level=1, axis=1).mean(axis=1),
-        ),
-        axis=1,
-    ).mean(axis=1)
-)
-cingulate_volc["log2_adpd"] = np.log2(
-    pd.concat(
-        (
-            cingulate.xs("adpd1", level=1, axis=1).mean(axis=1),
-            cingulate.xs("adpd2", level=1, axis=1).mean(axis=1),
-        ),
-        axis=1,
-    ).mean(axis=1)
-)
-pd.to_pickle(cingulate_volc, "data/interim/cingulate_volcano.pkl")
+# Save volcano data
+pd.to_pickle(frontal_volc, "data/interim/frontal_volc.pkl")
+pd.to_pickle(cingulate_volc, "data/interim/cingulate_volc.pkl")
 
-# Create red, black green custom color map
-cmap = LinearSegmentedColormap.from_list(
-    "Volcano", [(1, 0, 0), (0, 0, 0), (0, 1, 0)], N=3
-)
+# Plot data
+data = ['mean_ad', 'mean_pd', 'mean_adpd']
+for df in zip((frontal_volc, cingulate_volc), ('Frontal', 'Cingulate')):
+    for col in ('mean_ad', 'mean_pd', 'mean_adpd'):
+        df[0].volcano(col, 'mean_q_score', is_log=False, title=f'{df[1]} {col}')
+        plt.savefig(f"reports/figures/{df[1]}_{col}.png", dpi=600)
+        plt.close()
 
-# Set color column and plot
-cols_data = ("log2_ad", "log2_pd", "log2_adpd")
-cols_color = ("ad_color", "pd_color", "adpd_color")
-choices = [2, 0]
-for df in (frontal_volc, cingulate_volc):
-    fig, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
-    fig.suptitle(df.name)
-    fig.text(0.5, 0.05, "log2(fold change)", ha="center", va="center", fontsize=8)
-    fig.text(
-        0.05,
-        0.5,
-        "-log10(q-score)",
-        ha="center",
-        va="center",
-        rotation="vertical",
-        fontsize=8,
-    )
-    for items in zip(cols_data, cols_color, ax):
-        # Creates color column
-        conditions = [
-            (df["-log10_q"] >= 1.30103) & (df[items[0]] >= 0.5),
-            (df["-log10_q"] >= 1.30103) & (df[items[0]] <= -0.5),
-        ]
-        df[items[1]] = np.select(conditions, choices, default=1)
-        # Creates volcano plots
-        items[2].scatter(df[items[0]], df["-log10_q"], c=df[items[1]], cmap=cmap, s=1)
-        items[2].set_title(items[0].split("_")[1].upper(), fontsize=8)
-        items[2].set_xbound(lower=-2.1, upper=5.1)
-        items[2].set_xticks(np.arange(-2, 5.1, 1), minor=False)
-        items[2].set_xticks(np.arange(-2, 5.1, 0.25), minor=True)
-        items[2].set_xticklabels([-2, -1, 0, 1, 2, 3, 4, 5], fontsize=4)
-        items[2].set_ybound(lower=0, upper=6.1)
-        items[2].set_yticks(np.arange(0, 6.1, 1), minor=False)
-        items[2].set_yticks(np.arange(0, 6.1, 0.25), minor=True)
-        items[2].set_yticklabels([0, 1, 2, 3, 4, 5, 6], fontsize=4)
-    plt.savefig(f"reports/figures/{df.name}.png", dpi=600)
-    plt.close()
 print("Volcano Plots Made!")
